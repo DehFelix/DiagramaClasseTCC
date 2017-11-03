165a9f7740c970d6f58cafc6d2236ba2ad16f090
André Felix <andre.pfantonio@gmail.com>
2017-11-02 23:47:30 -0200

    Primeira Iteração

---
+++ DiagramaClasse/IDestilacao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiagramaClasse
+{
+    public interface IDestilacao
+    {
+        /// <summary>
+        /// Número de pratos teoricos
+        /// </summary>
+        double numeroPratosTeorico { get; set; }
+        /// <summary>
+        /// Número de pratos reais
+        /// </summary>
+        int numeroPretosPratica { get; set; }
+        /// <summary>
+        /// ???Refluxo???
+        /// </summary>
+        double refluxo { get; set; }
+        /// <summary>
+        /// Fluido no topo
+        /// </summary>
+        Fluido flidoDest { get; set; }
+        /// <summary>
+        /// Fluido no fundo
+        /// </summary>
+        Fluido fluidoBott { get; set; }
+        /// <summary>
+        /// Fluido de entrada
+        /// </summary>
+        Fluido fluidoFeed { get; set; }
+        /// <summary>
+        /// Esagio de alimentação
+        /// </summary>
+        int estagioFeed { get; set; }
+    }
+}
---
+++ DiagramaClasse/ITrocadorDeCalor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiagramaClasse
+{
+    public interface ITrocadorDeCalor
+    {
+        /// <summary>
+        /// Fluido frio na entrada
+        /// </summary>
+        Fluido fluidoQuenteEntrada { get; set; }
+        /// <summary>
+        /// Fluido quente na entrada
+        /// </summary>
+        Fluido fluidoFrioEntrada { get; set; }
+        /// <summary>
+        /// Coeficiente global de troca térmica (U) (J/m^1 k)
+        /// </summary>
+        double coefGlobalTerm { get; set; }
+        /// <summary>
+        /// Área de troca térmica (A) (m^2)
+        /// </summary>
+        double area { get; set; }
+        /// <summary>
+        /// Tipo de configuração ("contracorrente" ou "co-corrente")
+        /// </summary>
+        string tipoEscoamento { get; set; }
+        /// <summary>
+        /// Fluido frio na saida
+        /// </summary>
+        Fluido fluidoFrioSaida { get; set; }
+        /// <summary>
+        /// Fluido quente na saida
+        /// </summary>
+        Fluido fluidoQuenteSaida { get; set; }
+        /// <summary>
+        /// Calor transferido pelo trocador (J)
+        /// </summary>
+        int calorTransferido { get; set; }
+    }
+}
